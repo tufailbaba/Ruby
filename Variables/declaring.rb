635e807872d07b0186c8372e1fe15ea4608afb8e
Tufail <tufailbabacoc@gmail.com>
2025-10-07 17:18:51 +0530

    Variables and constants

---
+++ Variables/declaring.rb
@@ -0,0 +1,18 @@
+# Declaring variables in Ruby
+a = 10
+b = 20
+# a = 15
+
+#Memory address of variable
+puts a.object_id
+puts b.object_id
+#Adding two variables
+puts a + b
+
+str = "Hi ,Tufail"
+puts str
+#Declaring constants in Ruby
+A = 10
+B = 20
+# A = 12
+puts A + B
