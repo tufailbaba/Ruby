8996db0d76be930ee2045ca1bb737e6822896c66
Tufail <tufailbabacoc@gmail.com>
2025-10-31 09:19:51 +0530

    Str in Var

---
+++ Strings_Variables/var.rb
@@ -0,0 +1,15 @@
+first_name = "Tufail"
+# puts first_name
+
+# puts "Hello " + first_name
+
+last_name = 'Baba'
+
+# puts "Hello " +first_name +last_name
+# puts "Hello " +first_name + " "+last_name
+
+full_name = first_name +" " + last_name
+
+puts "Hello " +full_name
+
+
