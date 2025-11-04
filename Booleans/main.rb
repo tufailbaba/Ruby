c8854e2f56a32542f4655c89226fe1c27ff019a8
Tufail <tufailbabacoc@gmail.com>
2025-11-04 11:53:41 +0530

    Boolens

---
+++ Booleans/main.rb
@@ -0,0 +1,11 @@
+person_is_able_to_vote = true
+
+if person_is_able_to_vote then puts "You can vote" end
+
+person_age = 20
+if person_age >= 18
+  puts"You can vote!"
+  else
+    puts "You cant vote"
+  end
+  
