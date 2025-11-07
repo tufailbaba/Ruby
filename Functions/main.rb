d569c364ecffdb8a5f4e340286ab339b09b394bf
Tufail <tufailbabacoc@gmail.com>
2025-11-07 08:48:26 +0530

    Functions

---
+++ Functions/main.rb
@@ -0,0 +1,11 @@
+#function allow you to name a blok of code and reuse it later
+def square(number)
+  puts number * number
+end
+
+def cube(number)
+  puts number * number * number
+end
+
+square(5)
+cube(3)
