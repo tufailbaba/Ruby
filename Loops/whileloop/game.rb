e282d924d38ded4aba15018fe8b37111cbef2a58
Tufail <tufailbabacoc@gmail.com>
2025-11-19 11:27:05 +0530

    While loops

--- Loops/whileloop/game.rb
+++ Loops/whileloop/game.rb
@@ -1,7 +1,9 @@
 secret_word = "jaani"
 user_input = ""
+user_input_count = 0
 while user_input != secret_word
   puts "Enter the secret Word"
   user_input = gets.chomp().downcase
+  user_input_count +=1
 end
 puts "You are khandu"
