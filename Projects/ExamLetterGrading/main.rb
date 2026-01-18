e28e6d3021ff044c428cc7000b8445cae192cf6c
Tufail <tufailbabacoc@gmail.com>
2026-01-18 12:58:29 +0530

    Simple Version

--- Projects/ExamLetterGrading/main.rb
+++ Projects/ExamLetterGrading/main.rb
@@ -1,12 +1,20 @@
-puts"Enter your score (0-100):"
+#In Case statement
+puts "Enter your score (0-100):"
 score = gets.chomp.to_i
-result = case score
-when 90..100 then "Letter Grade A"
-when 80..90 then "Letter Grade B"
-when 70..80 then "Letter Grade C"
-when 60..70 then "Letter Grade D"
-when 50..60 then "Letter Grade E"
-when 0..50 then "Letter Grade E"
-  else 'InputInvalid '
+
+case score
+when 90..100
+  puts "Letter Grade A"
+when 80...90
+  puts "Letter Grade B"
+when 70...80
+  puts "Letter Grade C"
+when 60...70
+  puts "Letter Grade D"
+when 50...60
+  puts "Letter Grade E"
+when 0...50
+  puts "Letter Grade F"
+else
+  puts "Invalid Input"
 end
-puts result
