76377df6804682ab22cc26ff27fd0978d6a1676b
Tufail <tufailbabacoc@gmail.com>
2026-01-21 12:11:27 +0530

    With defualt parameters

--- Methods/main2.rb
+++ Methods/main2.rb
@@ -14,3 +14,11 @@ def function (name , age)
   puts "Your name is #{name} and your age is #{age}"
 end
 function("Tufail",21)
+
+#Methods with default parameters
+def language (lan1= "Ruby" , lan2= "Python")
+  puts "#{lan1} is a Programming language"
+  puts "#{lan2} is also a Programming language"
+end
+language()
+language("C","C++")
