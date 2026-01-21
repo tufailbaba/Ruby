81d9def8b0b67aa68716208d0bd408c774efed61
Tufail <tufailbabacoc@gmail.com>
2026-01-21 11:55:35 +0530

    Methods with arguments

--- Methods/main2.rb
+++ Methods/main2.rb
@@ -3,9 +3,14 @@ def name
 puts ("Hi Tufail")
 end
 name()
-
 #Methods with parameters
 def addition(a,b)
   puts a+b
 end
 addition(5,6)
+
+#More examples
+def function (name , age)
+  puts "Your name is #{name} and your age is #{age}"
+end
+function("Tufail",21)
