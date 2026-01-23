bbb288efc72ec1fd7e24ddc0a6da50516157f1f3
Tufail <tufailbabacoc@gmail.com>
2026-01-23 09:32:05 +0530

    While loops

---
+++ Conditional/until/main.rb
@@ -0,0 +1,16 @@
+i = 5
+n = 10
+until i >= n
+  puts "Kyasa lala"
+  i+=1
+end
+
+#until modifier
+ i = 5
+n = 10
+begin
+ 
+  puts "Kyasa lala"
+  i+=1
+end until i >= n
+
---
+++ Conditional/while/main.rb
@@ -0,0 +1,6 @@
+$i = 0
+$n = 5
+while $i <=$n do
+  puts "Here are the numbers #$i"
+  $i+=1
+end
