2e1241092723dfc0d816acd5bd16cc08352b9040
Tufail <tufailbabacoc@gmail.com>
2026-01-23 08:42:11 +0530

    Attr_accessor

--- GettersNsetters/main2.rb
+++ GettersNsetters/main2.rb
@@ -10,3 +10,19 @@ cat1.name = "Billa"
 cat1.color = "Golden"
 cat1.age = 21
 puts " #{cat1.name} is my cats name and he is #{cat1.age} is old and he is #{cat1.color} in colour  "
+
+#Now throght attr_accerssor method
+
+class Car
+  attr_accessor :name, :model , :engine
+  def to_s
+    puts "I have #{@name} and thats #{@model} and that have #{engine}ltr engine"
+    end
+end
+
+mycar = Car.new
+mycar.name = "Mustang"
+mycar.model = 2023
+mycar.engine = 2.5
+
+puts mycar
