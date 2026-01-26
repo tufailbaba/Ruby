43aff03b94d9d1a862ca26036b52ec5ad4b049c4
Tufail <tufailbabacoc@gmail.com>
2026-01-26 12:39:50 +0530

    Redo statements

--- NextRedo/main.rb
+++ NextRedo/main.rb
@@ -2,6 +2,7 @@ for i in 0..5
   if i < 2
     
     next
+    #If True then all the statements below dont gets executed
   end
   puts "Value = #{i}"
 end
---
+++ NextRedo/main2.rb
@@ -0,0 +1,8 @@
+for i in 1..5
+  if i < 2
+    puts "Value = #{i}"
+  end
+  redo
+  #Now it will redo it again and again
+
+end
