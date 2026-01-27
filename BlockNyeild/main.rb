74f726bbd7448ea71e66cefe7f9aa2bba0fd7dbc
Tufail <tufailbabacoc@gmail.com>
2026-01-27 11:14:01 +0530

    Block without Parameter

--- BlockNyeild/main.rb
+++ BlockNyeild/main.rb
@@ -1,4 +1,4 @@
-#Block without parameters 
+#Block 
 def sayhi()
   puts "You are in block"
   yield
@@ -7,3 +7,13 @@ sayhi {
   puts"you are in yeild" 
 }
 
+#Block without parameter
+
+def sayhello()
+  puts "Hey this is method"
+  yield 
+  puts "Its the end portion of block"
+end
+sayhello{
+  puts "Its the yeild portion of block"
+}
