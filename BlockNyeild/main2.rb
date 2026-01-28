5bb4c8fd9f6c172cc84be0b3dec507f7f82a41e8
Tufail <tufailbabacoc@gmail.com>
2026-01-28 10:53:19 +0530

    More on block as a parameter

--- BlockNyeild/main2.rb
+++ BlockNyeild/main2.rb
@@ -14,3 +14,21 @@ end
 greet do |name, age|
   puts "Name: #{name}, Age: #{age}"
 end
+
+def func (&bb)
+  4.times do (bb.call) end
+end
+
+func{
+  puts "This is bb"
+}
+
+def demo(&b1)
+b1.call("Parameter-1",2)
+end
+demo{
+  |par,num| puts "This variable 1: #{par},and this is second#{num}"
+}
+demo do |par1,num1|
+  puts par1,num1
+  end
