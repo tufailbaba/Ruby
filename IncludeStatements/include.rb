9c49784c7b0b5bfed1096a7a8c5f22203ba35b95
Tufail <tufailbabacoc@gmail.com>
2026-01-30 11:25:19 +0530

    Without Include Statement

---
+++ IncludeStatements/include.rb
@@ -0,0 +1,5 @@
+require_relative "main.rb"
+
+puts Week::First_Day
+Week.weeks_in_months
+Week.weeks_in_year
---
+++ IncludeStatements/main.rb
@@ -0,0 +1,9 @@
+module Week
+  First_Day = "Sunday"
+  def Week.weeks_in_months
+    puts "There are 4 weeks in a month"
+  end
+  def Week.weeks_in_year
+    puts "There are 52 weeks in a year"
+  end
+end
