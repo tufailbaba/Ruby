2473b676db44c237dddcc55d5c887a49eeb8b466
Tufail <tufailbabacoc@gmail.com>
2026-01-30 11:37:34 +0530

    With Include statement

--- IncludeStatements/include.rb
+++ IncludeStatements/include.rb
@@ -1,5 +1,15 @@
 require_relative "main.rb"
-
-puts Week::First_Day
+class Deacde
+  include Week
+  @@no_of_years = 10
+  def no_of_moths
+    puts "Today is " + Week::First_Day
+    number = @@no_of_years*12
+    puts "There are "+ number.to_s+ " months in a Decade"
+  end
+end
+obj = Deacde.new
+obj.no_of_moths
+# puts Week::First_Day
 Week.weeks_in_months
 Week.weeks_in_year
