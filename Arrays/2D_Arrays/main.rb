ecb9ee7e4561de5c57d4b1249c3d040dc2c43dab
Tufail <tufailbabacoc@gmail.com>
2026-01-31 11:00:18 +0530

    Viewing Elements in Array

--- Arrays/2D_Arrays/main.rb
+++ Arrays/2D_Arrays/main.rb
@@ -1,7 +1,8 @@
 arr = [['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i']]
 arr.each do |x|
-  x.each do |y|
-    print y
-  end
-  puts
+  # x.each do |y|
+  #   print y
+  # end
+  # puts
+  puts x.join(",")
 end
