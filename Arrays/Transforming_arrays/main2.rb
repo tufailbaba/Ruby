34c8b2ecb70c968d45c8167e0d40e15dea785f27
Tufail <tufailbabacoc@gmail.com>
2026-01-31 09:57:04 +0530

    Examples on Arrays

---
+++ Arrays/Transforming_arrays/main2.rb
@@ -0,0 +1,9 @@
+#practicng array
+arr = Array.new(12,"Tufail")
+puts "#{arr}"
+
+fruits_1 = ["apple", "banana", "mango", "orange"]
+fruits_2 = ["Grapes", "Avacado","Pear"]
+mixed_fruits.upcase = fruits_1+fruits_2
+puts mixed_fruits if mixed_fruits.include?("Apple")
+
