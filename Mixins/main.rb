f21a9cf05ba4f13a849dd90ad2dcda5e93670544
Tufail <tufailbabacoc@gmail.com>
2026-01-31 09:37:54 +0530

    More Examples on Mixins

--- Mixins/main.rb
+++ Mixins/main.rb
@@ -24,3 +24,42 @@ s.a1
 s.a2
 s.b1
 s.b2
+
+#Another Example
+
+module Example_11
+  def ex12
+    puts "Hey man"
+  end
+  def ex13
+    puts "Do you love her"
+  end
+  def ex14 
+    puts "I dont know"
+  end
+end
+module Example_21
+  def ex22
+    puts "Dont blush man"
+  end
+  def ex23
+    puts "I know "
+  end
+  def ex24
+    puts "You love her"
+  end
+end
+
+class Loveher
+  include Example_11
+  include Example_21
+end
+obj1 = Loveher.new
+
+obj1.ex12
+obj1.ex13
+obj1.ex14
+obj1.ex22
+obj1.ex23
+obj1.ex24
+
