438385e8696e7b8eede250eee866dbda70bfaec0
Tufail <tufailbabacoc@gmail.com>
2026-01-31 10:01:24 +0530

    Some more examples on Array

--- Arrays/Transforming_arrays/main2.rb
+++ Arrays/Transforming_arrays/main2.rb
@@ -4,6 +4,11 @@ puts "#{arr}"
 
 fruits_1 = ["apple", "banana", "mango", "orange"]
 fruits_2 = ["Grapes", "Avacado","Pear"]
-mixed_fruits.upcase = fruits_1+fruits_2
+mixed_fruits= fruits_1+fruits_2
 puts mixed_fruits if mixed_fruits.include?("Apple")
 
+
+array = [1,2,3,4,5]
+puts array.reverse
+puts array.sort
+puts array.sort.reverse
