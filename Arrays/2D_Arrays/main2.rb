9a11c8f71b0aee1d1baa72ed8b97879e8369864d
Tufail <tufailbabacoc@gmail.com>
2026-01-31 11:15:54 +0530

    More example on 2D

--- Arrays/2D_Arrays/main2.rb
+++ Arrays/2D_Arrays/main2.rb
@@ -9,3 +9,15 @@ end
 for row in Box
   puts (row.inspect)
 end
+
+
+NewBox = []
+7.times do |rows|
+  NewBox[rows]=[]
+  10.times do 
+    NewBox[rows] << 0
+  end
+end
+for rows in NewBox
+  puts (rows.inspect)
+end
