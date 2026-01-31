6485e3ed7191b38650589d7d26fd2ef0da91f575
Tufail <tufailbabacoc@gmail.com>
2026-01-31 10:19:38 +0530

    Methods in Array

---
+++ Arrays/Transforming_arrays/main3.rb
@@ -0,0 +1,17 @@
+# arr = ['H','E','L','L','O','W','O','R','L','D']
+# # puts (arr[-5,5].reverse)
+# puts ("**")
+# puts (arr[-5,5])
+# puts ("**")
+# puts (arr[0..4])
+
+# arr.delete_at(0)
+# arr.delete('O')
+# arr.shift
+arr1 = [1,2,3,4,5]
+# arr = arr1.map {|x| x * 2}  
+arr2 = [2,3,4,2,4]
+# arr3 = arr1-arr2 removes common
+# arr3 = arr1|arr2 Removes duplicates
+# arr3 = arr1&arr2 Shows common elements
+puts arr3
