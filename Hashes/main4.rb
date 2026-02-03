f53156169de0b4245ceefdb7c25a61a5076a199d
Tufail <tufailbabacoc@gmail.com>
2026-02-03 09:47:21 +0530

    Nested Hashes

---
+++ Hashes/main4.rb
@@ -0,0 +1,13 @@
+sports = {
+  "name" => "Tufail",
+  "age" => 21,
+  "passion" => {
+    "name1" => "Tufi"
+  }
+}
+
+puts sports.length
+puts sports.keys
+sports.each do |x , y| 
+  puts x ,y 
+end
