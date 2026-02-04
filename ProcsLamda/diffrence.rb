43e72213e83e251543b559ec6fac8dddfe442f2e
Tufail <tufailbabacoc@gmail.com>
2026-02-04 15:28:53 +0530

    Without retrun in lamda

--- ProcsLamda/diffrence.rb
+++ ProcsLamda/diffrence.rb
@@ -1,5 +1,5 @@
 #Diffrence Between  Proc and Lambda in Ruby
-
+#With and without Return keyword in Proc
 def my_method
   puts "Before Proc"
   my_proc = proc{
@@ -11,3 +11,16 @@ def my_method
   
 end
 my_method
+
+#With and without keywrod in lamda
+def my_method1
+  puts "Before Proc"
+  my_proc = lambda{
+    puts "Inside proc"
+    return
+  }
+  my_proc.call
+    puts "After proc"
+  
+end
+my_method1
