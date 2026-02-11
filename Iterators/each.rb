40b356a03ace8b1e3f0388b7a61357eece9390ea
Tufail <tufailbabacoc@gmail.com>
2026-02-11 11:41:27 +0530

    map and select iterators

---
+++ Iterators/each.rb
@@ -0,0 +1,6 @@
+arr = [1, 2, 3, 4, 5]
+ result = arr.map 
+ arr1 = [6,7,8,9,0]
+ result2 = arr1.map
+arr.join(arr1)
+puts arr
---
+++ Iterators/select.rb
@@ -0,0 +1,3 @@
+array  = [1,2,4,3,2,4]
+puts array.select{|n| n.even? }
+# puts array
